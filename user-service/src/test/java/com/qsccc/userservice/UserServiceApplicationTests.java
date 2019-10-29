57e9ea208b476dc7dbf972ba49685d9bfd1c140f
cp980725 <907718468@qq.com>
2019-10-29 11:42:12 +0800

    userservice

---
+++ user-service/src/test/java/com/qsccc/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.qsccc.userservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
