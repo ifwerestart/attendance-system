02fde5bcf4f42af067492629b09429904d6e30f8
cp980725 <907718468@qq.com>
2019-10-31 09:51:53 +0800

    employee

---
+++ employee-service/src/test/java/com/qxccc/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.qxccc.employeeservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
